061e99f407c86e2909ecf56c55e016f9b0e3382d
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-28 14:12:01 -0400

    Image slider was created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { SliderData } from './SliderData'
 
 import '../styles/Home.css'
 import Header from './Header'
+import ImageSlider from './ImageSlider'
 
 function Home() {
     return (
@@ -11,6 +13,7 @@ function Home() {
                 <span className='search-icon'><i className="fas fa-search"></i></span>
                 <input className='search' type="text" placeholder='Buscar...'/>
             </div>
+            <ImageSlider slides={SliderData} />
         </div>
     )
 }
---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1458819757519-7581bade511d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3JvY2tlcnl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1573573226985-72f731413b23?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFpciUyMGNvbmRpdGlvbmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1512203968542-2da613c1bb59?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmxlbmRlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1596793503406-d90c450ba1b1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njh8fGhvbWUlMjBlbnRlcnRhaW5tZW50fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1458245201577-fc8a130b8829?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGdhcmRlbiUyMHRvb2xzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8a2l0Y2hlbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1551516594-56cb78394645?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZ1cm5pdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1582787264299-66a617fcc6bf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvbWUlMjBlbnRlcnRhaW5tZW50fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+]
